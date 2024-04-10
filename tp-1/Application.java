50c55c57e528ce058da3a066553dd7161b137f63
Quentin DAVIN <84854854+itsax404@users.noreply.github.com>
2024-04-10 20:26:44 +0200

    Fin de la partie Premier Programme du TP 1

--- tp-1/Application.java
+++ tp-1/Application.java
@@ -1,7 +1,9 @@
 public class Application{
 
     public static void main(String[] args){
-        System.out.println("On fait du Java à Polytech Dijon !");
+        // System.out.println("On fait du Java à Polytech Dijon !");
+        Coordinate coords = new Coordinate(1, 9);
+        System.out.println(coords);
     }
 
 }
---
+++ tp-1/Coordinate.java
@@ -0,0 +1,25 @@
+public class Coordinate {
+    
+    private double x;
+    private double y;
+
+    public Coordinate(){
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Coordinate(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Coordinate(Coordinate coordonnees){
+        this.x = coordonnees.x;
+        this.y = coordonnees.y;
+    }
+
+    public String toString(){
+        return "( " + this.x + " ; " + this.y + " )";
+    }
+
+}
