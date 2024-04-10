e94fe2c982440481f8e2a559a57349f3246f1cee
Quentin DAVIN <84854854+itsax404@users.noreply.github.com>
2024-04-10 07:15:20 +0000

    Partie Réorganisation

--- tp-2/WebServer.java
+++ tp-2/WebServer.java
@@ -7,40 +7,13 @@ import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 
 public class WebServer {
-	private void readRequest(Socket socket){
-		try{
-			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			String response = "";
-			StringBuilder builder = new StringBuilder();
-			while((response = reader.readLine()) != null){
-				builder.append(response);
-			}
-			System.out.println(builder.toString());
-		}catch (IOException e){
-			e.printStackTrace();
-		}
-	}
-
-	private void sendResponse(Socket socket){
-		try{
-			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-			writer.write("HTTP/1.0 200 OK\r\n");
-			writer.flush();
-			writer.close();
-		}catch(IOException e){
-			e.printStackTrace();
-		}
-	}
 
 	public void run(int portNumber){
 		try{
 			ServerSocket socket = new ServerSocket(portNumber);
 			while(true){
 				Socket clientSocket = socket.accept();
-				this.readRequest(clientSocket);
-				this.sendResponse(clientSocket);
-
-				clientSocket.close();
+				RequestProcessor requestProcessor = new RequestProcessor(clientSocket);
 			}
 		} catch(IOException e){
 			e.printStackTrace();
