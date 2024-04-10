05ee19c5fcbe6316b0b4517032e53f4d1a23994c
Quentin DAVIN <84854854+itsax404@users.noreply.github.com>
2024-04-10 21:09:42 +0200

    Fin de la partie Liste du TP 1

--- tp-1/Application.java
+++ tp-1/Application.java
@@ -1,18 +1,25 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Application{
 
     public static void main(String[] args){
-        // System.out.println("On fait du Java à Polytech Dijon !");
-        // Coordinate coords = new Coordinate(1, 9);
-        // Point point = new Point(1, 9);
-        // System.out.println(point);
-        // Vector vector = new Vector(1, 1);
-        // point.move(vector);
-        // System.out.println(point);
+        /* 
+        System.out.println("On fait du Java à Polytech Dijon !");
+        Coordinate coords = new Coordinate(1, 9);
+        Point point = new Point(1, 9);
+        System.out.println(point);
+        Vector vector = new Vector(1, 1);
+        point.move(vector);
+        System.out.println(point);
+        */
 
+        /* 
         ScannerTools entree = new ScannerTools(); 
         print("Veuillez saisir une chaîne de caractères : ");
+        */
+
+        /*
         String stringEntree = entree.nextLine();
         print("Veuillez saisir un entier : ");
         int integerEntree = entree.nextInt();
@@ -22,6 +29,27 @@ public class Application{
         print("La chaine de caractère entréé : " + stringEntree);
         print("L'entier entrée : " + integerEntree);
         print("le double entrée : " + doubleEntree);
+         */
+
+        ScannerTools entree = new ScannerTools(); 
+        print("Veuillez saisir le nombre de points que vous voulez crée : ");
+        int nbPoints = entree.nextInt();
+        ArrayList<Point> points = new ArrayList<>();
+
+        for(int i = 0; i < nbPoints; i++ ){
+            print("Veuillez entrer la composante x du point "+(i+1) + " :");
+            int x = entree.nextInt();
+            print("Veuillez entrer la composante y du point "+(i+1) + " :");
+            int y = entree.nextInt();
+
+            points.add(new Point(x, y));
+        }
+        
+        print((points.size()) + " points entrées : ");
+        for(int i = 0; i < points.size(); i++){
+            print("Point " + (i+1) + " : " + points.get(i));
+        }
+
     }
 
     private static void print(String message){
